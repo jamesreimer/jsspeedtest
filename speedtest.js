b8374b26a69f48c1a97fa41b3eb9260712932a9a
James Reimer <james.reimer@bubblecubestudios.com>
2014-11-26 11:18:48 -0600

    Create speedtest.js

---
+++ speedtest.js
@@ -0,0 +1,22 @@
+function SpeedTest(testImplement, testParams, repetitions) {
+  this.testImplement = testImplement;
+  this.testParams = testParams;
+  this.repetitions = repetitions || 10000;
+  this.avaerage = 0;
+}
+
+SpeedTest.prototype = {
+  startTest: function() {
+    var beginTime, endTime, sumTimes = 0;
+    for (var i = 0, x = this.repetitions; i < x; i++) {
+      beginTime = +new Date();
+      this.testImplement(this.testParams);
+      endTime = +new Date();
+      sumTimes += endTime - beginTime;
+    }
+    this.average = sumTimes / this.repetitions;
+    return console.log("Average execution across " +
+                       this.repetitions + ": " +
+                       this.average);
+  }
+}
